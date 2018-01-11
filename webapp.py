2bd2fffa67cada424f4bdaa4198a6f0c1a4c9dc5
Alberto GIl <alberto8gil@gmail.com>
2018-01-11 10:01:25 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,6 +6,13 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('home.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+ def login():
+ if request.method == 'POST':
+     do_the_login()
+ else:
+    show_the_login_form()
+
 @app.route("/response")
 def render_response():
     color = request.args["color"]
